63e2ff8e2fb921a74f789155c5c79f32a8823759
DanielSobrino <daniel.sobrino.7e7@itb.cat>
2024-01-03 13:37:59 +0100

    Segundo commit

--- src/main/kotlin/FuncionsPreu.kt
+++ src/main/kotlin/FuncionsPreu.kt
@@ -1,5 +1,11 @@
 import java.util.*
 
+/**
+ * Función que calcula el precio del billete basado en la cantidad de zonas.
+ * @param zones Número de zonas seleccionadas.
+ * @param preuBase Precio base para el cálculo.
+ * @return Precio calculado según la cantidad de zonas.
+ */
 fun CalcularPreu(zones: Int, preuBase: Double): Double {
     return when (zones) {
         1 -> preuBase
@@ -8,49 +14,69 @@ fun CalcularPreu(zones: Int, preuBase: Double): Double {
         else -> 0.0
     }
 }
+
+/**
+ * Función para comprar un billete sencillo.
+ */
 fun BitlletSenzill() {
     val PreuBase = 2.40
-    println("Has seleccionat comprar un Bitllet senzill.")
+    println("${colorAzul()}Has seleccionat comprar un Bitllet senzill.")
     val zones = numeroZones()
     val mostrarPreu = CalcularPreu(zones, PreuBase)
     comprarBitllet("BitlletSenzill", mostrarPreu)
 }
 
+/**
+ * Función para comprar una tarjeta TCasual.
+ */
 fun TCasual() {
-    println("Has seleccionat la tarjeta TCasual")
-    val PreuBase= 11.35
-    val zones= numeroZones()
-    val mostrarPreu= CalcularPreu(zones, PreuBase)
+    println("${colorRojo()}Has seleccionat la tarjeta TCasual")
+    val PreuBase = 11.35
+    val zones = numeroZones()
+    val mostrarPreu = CalcularPreu(zones, PreuBase)
     comprarBitllet("TCasual", mostrarPreu)
 }
 
+/**
+ * Función para comprar una tarjeta TUsual.
+ */
 fun TUsual() {
-    println("Has seleccionat la tarjeta TUsual")
-    val PreuBase= 40.00
-    val zones= numeroZones()
-    val mostrarPreu= CalcularPreu(zones, PreuBase)
+    println("${colorAzul()}Has seleccionat la tarjeta TUsual")
+    val PreuBase = 40.00
+    val zones = numeroZones()
+    val mostrarPreu = CalcularPreu(zones, PreuBase)
     comprarBitllet("TUsual", mostrarPreu)
 }
 
+/**
+ * Función para comprar una tarjeta TFamiliar.
+ */
 fun TFamiliar() {
-    println("Has seleccionat la tarjeta TFamiliar")
-    val PreuBase= 10.00
-    val zones= numeroZones()
-    val mostrarPreu= CalcularPreu(zones, PreuBase)
+    println("${colorRojo()}Has seleccionat la tarjeta TFamiliar")
+    val PreuBase = 10.00
+    val zones = numeroZones()
+    val mostrarPreu = CalcularPreu(zones, PreuBase)
     comprarBitllet("TFamilar", mostrarPreu)
 }
 
+/**
+ * Función para comprar una tarjeta TJove.
+ */
 fun TJove() {
-    println("Has seleccionat la tarjeta TJove")
-    val PreuBase= 80.00
-    val zones= numeroZones()
-    val mostrarPreu= CalcularPreu(zones, PreuBase)
+    println("${colorAzul()}Has seleccionat la tarjeta TJove")
+    val PreuBase = 80.00
+    val zones = numeroZones()
+    val mostrarPreu = CalcularPreu(zones, PreuBase)
     comprarBitllet("TJove", mostrarPreu)
 }
 
+/**
+ * Función para realizar la compra de un billete o tarjeta.
+ * @param Titol Título del billete o tarjeta seleccionada.
+ * @param preuFinal Precio final a pagar.
+ */
 fun comprarBitllet(Titol: String, preuFinal: Double) {
-    println("El preu del teu $Titol és: $preuFinal €")
-
+    println("${colorLila()}El preu del teu $Titol és: $preuFinal €")
     var pagament = 0.0
     val EfectiuPermes = listOf(0.05, 0.10, 0.20, 0.50, 1.00, 2.0, 5.0, 10.0, 20.0, 50.0)
 
@@ -62,16 +88,19 @@ fun comprarBitllet(Titol: String, preuFinal: Double) {
             println("ERROR: Introduïu una moneda o bitllet vàlid.")
             continue
         }
-       pagament += moneda
+        pagament += moneda
     }
     val canvi = pagament - preuFinal
-    println("Has completat la transacció. Aquest es el teu canvi: $canvi €")
+    println("${colorAzul()}Has completat la transacció. Aquest es el teu canvi: $canvi €")
     demanarTiquet()
 }
 
+/**
+ * Función para solicitar si el usuario desea obtener un tiquet.
+ */
 fun demanarTiquet() {
     val respostaUsuari = llegirString("Vols el tiquet? (si/no): ", listOf("si", "no"))
     if (respostaUsuari == "si") {
-        println("Estem imprimint el tiquet, esperi un moment")
+        println("${colorRojo()}Estem imprimint el tiquet, esperi un moment")
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,17 @@
 import java.util.*
 
+fun colorAzul() = "\u001B[34m"
+fun colorRojo() = "\u001B[31m"
+fun colorLila() = "\u001B[35m"
+
+
+
+/**
+ * Función principal que ejecuta la lógica de compra de billetes hasta un máximo de 3 intentos.
+ * Muestra un menú de opciones y permite al usuario seleccionar el tipo de billete a comprar.
+ */
 fun main() {
-    var bitllets=0
+    var bitllets = 0
     while (bitllets < 3) {
         MenuBitllets()
         val Bitllet = demanarOpcio()
@@ -12,19 +22,22 @@ fun main() {
             3 -> TUsual()
             4 -> TFamiliar()
             5 -> TJove()
-            0 -> println("Has finalitzat la compra. Gràcies per utilitzar aquesta maquina. Fins Aviat!!!")
+            0 -> println("Has finalitzat la compra. Gràcies per utilitzar aquesta màquina. Fins Aviat!!!")
             else -> println("Aquesta opció no és valida, torna a intentar-ho!!")
-            }
+        }
         bitllets++
     }
-    if (bitllets > 3) {
+    if (bitllets >= 3) {
         println("Has arribat al límit de transaccions possibles. Fins Aviat!!!")
     }
 }
 
+/**
+ * Función para mostrar el menú de opciones disponibles de billetes.
+ */
 fun MenuBitllets() {
     println("\nBenvingut/da a la màquina de venda de bitllets del transport metropolità de Barcelona.")
-    println("Selecciona el tipus de bitllet que desitges comprar:")
+    println("${colorAzul()}Selecciona el tipus de bitllet que desitges comprar:")
     println("1. Bitllet senzill")
     println("2. TCasual")
     println("3. TUsual")
@@ -33,12 +46,18 @@ fun MenuBitllets() {
     println("0. Sortir")
 }
 
+/**
+ * Función para solicitar al usuario que introduzca una opción válida del menú.
+ * @return Opción seleccionada por el usuario.
+ */
 fun demanarOpcio(): Int {
-    return llegirInt("Introdueix el número del bitllet que vols comprar: ",
-        0,
-        5)
+    return llegirInt("Introdueix el número del bitllet que vols comprar: ", 0, 5)
 }
 
+/**
+ * Función que solicita al usuario seleccionar el número de zonas para el billete.
+ * @return Número de zonas seleccionadas.
+ */
 fun numeroZones(): Int {
     return llegirInt(
         "\nSelecciona les zones de distància que vols visitar:\n1. Zona 1\n2. Zona 2\n3. Zona 3\n Introdueix el número de zones: ",
